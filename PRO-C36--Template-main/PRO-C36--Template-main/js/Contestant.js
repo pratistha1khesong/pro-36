9bb12ed05cde7e06cd42523a045563decfacd5ef
pratistha1khesong <82515280+pratistha1khesong@users.noreply.github.com>
2021-10-06 17:10:38 +0530

    Add files via upload

---
+++ PRO-C36--Template-main/PRO-C36--Template-main/js/Contestant.js
@@ -0,0 +1,10 @@
+class Contestant {
+  constructor(){
+   // this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    // this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    //this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    // question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+  }
+
+  
+}
